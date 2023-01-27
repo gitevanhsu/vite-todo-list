f58701c48f21c12a62039dca2073fa632e3e4e72
Evan <aqwe78965@gmail.com>
2023-01-27 14:37:43 +0800

    Inject data, and show todo list on page with clear and trigger completed function.

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,12 +1,54 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import UserInfo from "../../context/userContext";
 import Plus from "/plus 1.png";
 import Vector from "/vector-x.png";
 
+interface TodoItemType {
+  todo: string;
+  completed: boolean;
+}
+
+const data = [
+  {
+    todo: "把冰箱發霉的檸檬拿去丟",
+    completed: false,
+  },
+  {
+    todo: "打電話叫媽媽匯款給我",
+    completed: true,
+  },
+  {
+    todo: "整理電腦資料夾",
+    completed: false,
+  },
+  {
+    todo: "繳電費水費瓦斯費",
+    completed: true,
+  },
+  {
+    todo: "約vicky禮拜三泡溫泉",
+    completed: false,
+  },
+  {
+    todo: "約ada禮拜四吃晚餐",
+    completed: false,
+  },
+];
+
 export default function TodoPage() {
   const { member } = useContext(UserInfo);
+  const [todoList, setTodoList] = useState(data);
+
+  const triggerCompleted = (index: number) => {
+    todoList[index].completed = !todoList[index].completed;
+    setTodoList([...todoList]);
+  };
+
+  const clearCompleted = () => {
+    setTodoList((c) => c.filter((item) => !item.completed));
+  };
 
   return (
     <div className="relative min-h-screen pt-[50px] pb-[20px] my-todo-bg flex flex-col justify-center">
@@ -22,7 +64,6 @@ export default function TodoPage() {
             <img src={Plus} alt="plus" className="cursor-pointer" />
           </div>
         </div>
-
         <div className="sm:w-[500px] w-[311px] bg-white rounded-[10px] shadow-md mt-[16px] text-gray-700 text-sm">
           <ul className="flex font-semibold">
             <li className="w-[33.33333%] text-center py-[15px] cursor-pointer border-b-[2px] border-black">
@@ -37,142 +78,48 @@ export default function TodoPage() {
           </ul>
           <div>
             <ul className="pl-[24px]">
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item1"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item1"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
+              {todoList.map((item, index) => {
+                const { todo, completed } = item;
+                return (
+                  <li key={todo} className="flex items-center justify-between">
+                    <div className="py-[15px] border-b w-full">
+                      <label
+                        htmlFor={todo}
+                        className="flex items-center cursor-pointer"
+                      >
+                        <input
+                          readOnly
+                          id={todo}
+                          type="checkbox"
+                          value="true"
+                          checked={completed}
+                          className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
+                          onClick={() => triggerCompleted(index)}
+                        />
+                        <span>{todo}</span>
+                      </label>
+                    </div>
+                    <img
+                      src={Vector}
+                      alt="x"
+                      className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
                     />
-                    <span>把冰箱發霉的檸檬拿去丟</span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item2"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item2"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
-                      checked
-                    />
-                    <span className="line-through text-gray-400">
-                      打電話叫媽媽匯款給我
-                    </span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item3"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item3"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
-                    />
-                    <span>整理電腦資料夾</span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item4"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item4"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
-                      checked
-                    />
-                    <span className="line-through text-gray-400">
-                      繳電費水費瓦斯費
-                    </span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item5"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item5"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
-                    />
-                    <span>約vicky禮拜三泡溫泉</span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
-              <li className="flex items-center justify-between ">
-                <div className="py-[15px] border-b w-full">
-                  <label
-                    htmlFor="item6"
-                    className="flex items-center cursor-pointer"
-                  >
-                    <input
-                      id="item6"
-                      type="checkbox"
-                      value="true"
-                      className="w-[20px] h-[20px] mr-[16px] cursor-pointer"
-                    />
-                    <span>約ada禮拜四吃晚餐</span>
-                  </label>
-                </div>
-                <img
-                  src={Vector}
-                  alt="x"
-                  className="w-[14px] h-[14px] mx-[17px] cursor-pointer"
-                />
-              </li>
+                  </li>
+                );
+              })}
             </ul>
             <div className="flex items-center justify-between ml-[24px] mr-[48px] pt-[25px] pb-[32px]">
-              <p> 5 個已完成項目</p>
-              <p className="text-gray-400 cursor-pointer">清除已完成項目</p>
+              <p>
+                {todoList.filter((item) => item.completed).length}
+                個已完成項目
+              </p>
+              <button
+                onClick={clearCompleted}
+                type="button"
+                className="text-gray-400 cursor-pointer"
+              >
+                清除已完成項目
+              </button>
             </div>
           </div>
         </div>
